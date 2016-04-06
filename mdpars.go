7ec8c152a4f7a901027b9add45db90c44f184a6f
Leonid Nikolaev <nikonor@nikonor.ru>
2016-04-06 16:52:29 +0300

    получили список интересующих файлов

--- mdpars.go
+++ mdpars.go
@@ -1,17 +1,40 @@
 package main
 import (
     "fmt"
+    "io/ioutil"
+    "log"
+    "strings"
 )
 
 func init() {
     readConfig();
 }
 
+var (
+    default_dir = "/home/nikonor/Yandex.Disk/#iA/"
+    mdfiles []string
+)
+
+
 func readConfig() {
-    
+    fmt.Println("call readConfig");
 }
 
 func main() {
     fmt.Println("Start")
+    files, err := ioutil.ReadDir(default_dir)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    for _, file := range files {
+        n := file.Name()
+        if !file.IsDir() && strings.HasSuffix(n, ".md") {
+            mdfiles = append(mdfiles,n)
+        }
+    }    
+
+    fmt.Println(mdfiles);
+
     fmt.Println("Finish")
 }
