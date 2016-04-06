24731699e5fc1e6170cc494dba3b91809f7f304c
Leonid Nikolaev <nikonor@nikonor.ru>
2016-04-06 17:23:30 +0300

    получение данных и файла + подготовка к анализу

--- mdpars.go
+++ mdpars.go
@@ -4,6 +4,8 @@ import (
     "io/ioutil"
     "log"
     "strings"
+    "os"
+    "bufio"
 )
 
 func init() {
@@ -13,28 +15,80 @@ func init() {
 var (
     default_dir = "/home/nikonor/Yandex.Disk/#iA/"
     mdfiles []string
+    err error
 )
+
+    type mdstring struct {
+        Level int 
+        Text string
+        C int
+    }
 
 
 func readConfig() {
     fmt.Println("call readConfig");
 }
 
-func main() {
-    fmt.Println("Start")
+func getFiles () ([]string, error) {
+    var mdfs []string
     files, err := ioutil.ReadDir(default_dir)
-    if err != nil {
-        log.Fatal(err)
-    }
 
     for _, file := range files {
         n := file.Name()
         if !file.IsDir() && strings.HasSuffix(n, ".md") {
-            mdfiles = append(mdfiles,n)
+            mdfs = append(mdfs,n)
         }
     }    
+    return mdfs,err
+}
 
-    fmt.Println(mdfiles);
+func parseFile (fname string) (string,error) {
+    var (
+        ret string
+        filedata []mdstring
+        err error
+        file *os.File
+    )
+    ret = ret + "# "+fname
+    file, err = os.Open(default_dir+fname)
+    defer file.Close()
+    filedata,err = readMDFile(file)
+    fmt.Println(filedata);
+
+    return ret,err
+}
+
+func readMDFile(file *os.File) ([]mdstring,error) {
+    var (
+        ret []mdstring
+        err error
+        level = 1
+    )
+
+    scanner := bufio.NewScanner(file)
+    scanner.Split(bufio.ScanLines)   
+    for scanner.Scan() {
+        // fmt.Println(scanner.Text());
+        c := 22
+        ret = append(ret,mdstring{Level: level, Text: scanner.Text(), C: c})
+    }    
+
+    return ret,err
+}
+
+func main() {
+    fmt.Println("Start")
+
+    // получаем список файлов для обработки
+    mdfiles,err = getFiles()
+    if err != nil {
+        log.Fatal(err)
+    }
+    // обработка файлов
+    // TK - пока один
+    var o string
+    o,err = parseFile("ToDo.md")
+    fmt.Println("--- begin for ToDo.md ---\n",o,"\n--- end for ToDo.md ---");
 
     fmt.Println("Finish")
 }
